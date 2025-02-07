ade5c9eb87599c5aeb7cdf029b2d6387c3ccf619
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2025-02-07 12:19:33 -0300

    Adicionar suporte para operadores de comparação e if-else

--- Rust Compiler WIP/ExpressionLanguageLex.py
+++ Rust Compiler WIP/ExpressionLanguageLex.py
@@ -1,5 +1,6 @@
 import ply.lex as lex
 
+# Palavras reservadas
 reservadas = {
     'while': 'WHILE',
     'true': 'TRUE',
@@ -12,57 +13,65 @@ reservadas = {
     'else': 'ELSE',
     'for': 'FOR',
     'in': 'IN',
+    'range': 'RANGE'
 }
 
 # Tokens
 tokens = (
-    "PLUS", 
-    "MINUS", 
-    "TIMES", 
-    "DIVIDE", 
-    "LPAREN", 
-    "RPAREN",
-    "LBRACE",
-    "RBRACE",
-    "NUMBER",
-    "ID"
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'GREATER',
+    'LESS',
+    'EQUALS',
+    'LBRACE',
+    'RBRACE',
+    'ID',
+    'ASSIGN',
+    'COMMA',
+    'ARROW',
+    'SEMICOLON',
 ) + tuple(reservadas.values())
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LBRACE  = r'\{'
-t_RBRACE  = r'\}'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_GREATER = r'>'
+t_LESS = r'<'
+t_EQUALS = r'=='
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_ASSIGN = r'='
+t_COMMA = r','
+t_ARROW = r'->'
+t_SEMICOLON = r';'
+t_RANGE = r'\.\.'
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-# Identificador (ID), se for uma palavra reservada, retorna a palavra reservada
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reservadas.get(t.value, 'ID')  # Se for uma palavra reservada, retorna o nome
+    t.type = reservadas.get(t.value, 'ID')
     return t
 
-# Ignorar espaços e tabs
 t_ignore = ' \t'
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Comentários (não geram tokens)
-def t_COMMENT(t):
-    r'//.*|/\*(.|\n)*?\*/'
-    pass  # Simplesmente ignora os comentários
-
-# Erro de token
 def t_error(t):
     print(f"Caractere ilegal '{t.value[0]}'")
     t.lexer.skip(1)
-
+    
 lexer = lex.lex()
