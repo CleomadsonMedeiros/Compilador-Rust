f02c43c8afdf0f8a34e6810e041ce8ff3bd079e1
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2025-02-14 12:42:15 -0300

    Adicionar execução apenas dentro da main, múltiplo statements dentro da função, assignments, estrutura de repetição, condicionais

--- Rust Compiler/ExpressionLanguageLex.py
+++ Rust Compiler/ExpressionLanguageLex.py
@@ -1,118 +1,78 @@
 import ply.lex as lex
 
-# Reservadas
+# Palavras reservadas
 reservadas = {
     'while': 'WHILE',
     'true': 'TRUE',
     'false': 'FALSE',
     'return': 'RETURN',
-    'fn': 'FN', # Função
-    'let': 'LET', # Variável Imutável
-    'mut': 'MUT', # Variável Mutável
+    'fn': 'FN',
+    'let': 'LET',
+    'mut': 'MUT',
     'if': 'IF',
     'else': 'ELSE',
     'for': 'FOR',
     'in': 'IN',
-    'i32': 'I32',
-    'f64': 'F64'
+    'range': 'RANGE',
+    'main': 'MAIN'
 }
 
 # Tokens
 tokens = (
-    "PLUS", 
-    "MINUS", 
-    "TIMES", 
-    "DIVIDE", 
-    "EQUAL",
-    "LPAREN", 
-    "RPAREN",
-    "LBRACKET",
-    "RBRACKET",
-    "LBRACE",
-    "RBRACE",
-    "COMMA", 
-    "NUMBER", 
-    "ID",
-    "SEMICOLON",
-    "COLON",
-    "DOT",
-    "ARROW",
-    "AND",
-    "OR",
-    "NOT",
-    "LT", #Less Than
-    "GT", #Greater Than
-    "LE", #Less or Equal
-    "GE", #Greater or Equal
-    "EQ", #Equal
-    "NE", #Not Equal
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'GREATER',
+    'LESS',
+    'EQUALS',
+    'LBRACE',
+    'RBRACE',
+    'ID',
+    'ASSIGN',
+    #'COMMA',
+    #'ARROW',
+    'SEMICOLON'
 ) + tuple(reservadas.values())
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
+t_PLUS = r'\+'
+t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_EQUAL = r'='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LBRACKET = r'\['
-t_RBRACKET = r'\]'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_GREATER = r'>'
+t_LESS = r'<'
+t_EQUALS = r'=='
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
-t_COMMA = r','
+t_ASSIGN = r'='
+#t_COMMA = r','
+#t_ARROW = r'->'
 t_SEMICOLON = r';'
-t_COLON = r':'
-t_DOT = r'\.'
-t_ARROW = r'->'
-t_AND = r'&&'
-t_OR = r'\|\|'
-t_NOT = r'!'
-t_LT = r'<'
-t_GT = r'>'
-t_LE = r'<='
-t_GE = r'>='
-t_EQ = r'=='
-t_NE = r'!='
-
-def t_NUMBER_FLOAT(t):
-    r'\d+\.\d+'
-    t.value = float(t.value)
-    return t
+t_RANGE = r'\.\.'
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_STRING(t):
-    r'\"([^\\\n]|(\\.))*?\"'
-    return t
-
-# Identificador, caso seja uma palavra reservada, retorna a palavra reservada
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reservadas.get(t.value, 'ID')
     return t
 
-# Ignorar espaços em branco
 t_ignore = ' \t'
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Comentários com // ou /* */
-def t_COMMENT(t):
-    r'//.*|/\*(.|\n)*?\*/'
-    pass # Faz nada
-
 def t_error(t):
-    print("Caractere ilegal '%s'" % t.value[0])
+    print(f"Caractere ilegal '{t.value[0]}'")
     t.lexer.skip(1)
-
+    
 lexer = lex.lex()
-lexer.input("if else")
-
-print('{:10s}{:10s}{:10s}{:10s}'.format("Token", "Lexema", "Linha", "Coluna"))
-for tok in lexer:
-  print('{:10s}{:10s}{:10s}{:10s}'.format(tok.type, tok.value, str(tok.lineno), str(tok.lexpos)))
---
+++ Rust Compiler/ExpressionLanguageParser.py
@@ -0,0 +1,145 @@
+import ply.yacc as yacc
+from ExpressionLanguageLex import tokens
+
+# Regras do analisador sintático (parser)
+
+# Pra executar apenas dentro da função main
+def p_program_main(p):
+    'program : main_function'
+
+def p_main_function(p):
+    'main_function : FN MAIN LPAREN RPAREN block_statement'
+
+def p_statement_list(p):
+    '''statement_list : statement
+                     | statement statement_list'''
+#region statements
+def p_statement_expression_statement(p):
+    'statement : expression_statement'
+
+def p_statement_var_declaration(p):
+    'statement : var_declaration'
+
+def p_statement_var_assignment(p):
+    'statement : var_assignment'
+
+def p_if_statement(p):
+    'statement : IF LPAREN condition RPAREN LBRACE expression RBRACE'
+
+def p_if_else_statement(p):
+    'statement : IF LPAREN condition RPAREN LBRACE expression RBRACE ELSE LBRACE expression RBRACE'
+
+def p_statement_while_statement(p):
+    'statement : while_statement'
+
+def p_statement_return_statement(p):
+    'statement : return_statement'
+
+def p_statement_for_statement(p):
+    'statement : for_statement'
+
+def p_statement_block_statement(p):
+    'statement : block_statement'
+#endregion
+
+def p_expression_statement(p):
+    'expression_statement : expression SEMICOLON'
+
+def p_var_declaration(p):
+    'var_declaration : LET MUT ID ASSIGN expression SEMICOLON'
+
+def p_var_declaration2(p):
+    'var_declaration : LET ID ASSIGN expression SEMICOLON'
+
+def p_var_assignment(p):
+    'var_assignment : ID ASSIGN expression SEMICOLON'
+
+def p_while_statement(p):
+    'while_statement : WHILE LPAREN expression RPAREN block_statement'
+
+def p_for_statement(p):
+    'for_statement : FOR ID IN expression block_statement'
+
+def p_return_statement(p):
+    'return_statement : RETURN expression SEMICOLON'
+
+def p_block_statement_braces(p):
+    'block_statement : LBRACE statement_list RBRACE'
+
+def p_condition_greater(p):
+    'condition : expression GREATER expression'
+
+def p_condition_less(p):
+    'condition : expression LESS expression'
+
+def p_condition_equals(p):
+    'condition : expression EQUALS expression'
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    # p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    # p[0] = p[1] - p[3]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    # p[0] = p[1] * p[3]
+
+def p_term_divide(p):
+    'term : term DIVIDE factor'
+    # p[0] = p[1] / p[3] if p[3] != 0 else print("Erro: Divisão por zero")
+
+def p_expression_term(p):
+    'expression : term'
+    # p[0] = p[1]
+
+def p_expression_range(p):
+    'expression : expression RANGE term'
+
+def p_term_factor(p):
+    'term : factor'
+    # p[0] = p[1]
+
+def p_factor_number(p):
+    'factor : NUMBER'
+    # p[0] = p[1]
+
+def p_factor_paren(p):
+    'factor : LPAREN expression RPAREN'
+    # p[0] = p[2]
+
+def p_factor_boolean(p):
+    '''factor : TRUE
+              | FALSE'''
+
+def p_factor_id(p):
+    'factor : ID'
+
+def p_error(p):
+    print("Erro de sintaxe")
+    print(p)
+
+parser = yacc.yacc()
+
+# Teste do parser
+resultado = parser.parse('''                 
+    fn main(){
+        let mut x = 5;
+        x + 1;
+        let y = 4;
+        1 + 1;
+        3 - 2;
+        x = y + 4;
+        while (1) {
+        1 + 1;
+        2 / 2;
+        }
+        for x in 1..10 / 5 {
+        0 + 1;
+        }
+        return x;                 
+    }     
+''')
+print(resultado)
