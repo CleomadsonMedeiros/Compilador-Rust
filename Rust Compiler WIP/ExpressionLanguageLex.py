25f3f4ab86ccce886abfdfda3ee667deaed05a1b
renneb777 <rennerox46@gmail.com>
2025-02-17 16:05:29 -0300

    Adicionado Operadores Logicos e Relacionais

--- Rust Compiler WIP/ExpressionLanguageLex.py
+++ Rust Compiler WIP/ExpressionLanguageLex.py
@@ -1,5 +1,6 @@
 import ply.lex as lex
 
+# Palavras reservadas
 reservadas = {
     'while': 'WHILE',
     'true': 'TRUE',
@@ -12,63 +13,82 @@ reservadas = {
     'else': 'ELSE',
     'for': 'FOR',
     'in': 'IN',
+    'range': 'RANGE',
+    'main': 'MAIN'
 }
 
 # Tokens
 tokens = (
-    "PLUS", 
-    "MINUS", 
-    "TIMES", 
-    "DIVIDE", 
-    "LPAREN", 
-    "RPAREN",
-    "LBRACE",
-    "RBRACE",
-    "NUMBER",
-    "ID",
-    "SEMICOLON",
-    "ASSIGN",
-    "RANGE"
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'GREATER',
+    'LESS',
+    'EQUALS',
+    'LBRACE',
+    'RBRACE',
+    'ID',
+    'ASSIGN',
+    'NOTEQUAL',
+    'GREATEREQUAL',
+    'LESSEQUAL',
+    #'COMMA',
+    #'ARROW',
+    'SEMICOLON',
+    'MODULO',
+    'NOT',
+    'OR',
+    'AND'
 ) + tuple(reservadas.values())
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LBRACE  = r'\{'
-t_RBRACE  = r'\}'
-t_SEMICOLON = r';'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_GREATER = r'>'
+t_LESS = r'<'
+t_EQUALS = r'=='
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
 t_ASSIGN = r'='
+#t_COMMA = r','
+#t_ARROW = r'->'
+t_SEMICOLON = r';'
 t_RANGE = r'\.\.'
+t_NOTEQUAL = r'!='
+t_GREATEREQUAL = r'>='
+t_LESSEQUAL = r'<='
+t_MODULO = r'%'
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_NOT = r'!'
+
+
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-# Identificador (ID), se for uma palavra reservada, retorna a palavra reservada
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reservadas.get(t.value, 'ID')  # Se for uma palavra reservada, retorna o nome
+    t.type = reservadas.get(t.value, 'ID')
     return t
 
-# Ignorar espaços e tabs
 t_ignore = ' \t'
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Comentários (não geram tokens)
-def t_COMMENT(t):
-    r'//.*|/\*(.|\n)*?\*/'
-    pass  # Simplesmente ignora os comentários
-
-# Erro de token
 def t_error(t):
     print(f"Caractere ilegal '{t.value[0]}'")
     t.lexer.skip(1)
-
+    
 lexer = lex.lex()
--- Rust Compiler WIP/ExpressionLanguageParser.py
+++ Rust Compiler WIP/ExpressionLanguageParser.py
@@ -2,12 +2,18 @@ import ply.yacc as yacc
 from ExpressionLanguageLex import tokens
 
 # Regras do analisador sintático (parser)
-def p_program_statement(p):
-    'program : statement'
 
-def p_program_program_statement(p):
-    'program : program statement'
+# Pra executar apenas dentro da função main
+def p_program_main(p):
+    'program : main_function'
 
+def p_main_function(p):
+    'main_function : FN MAIN LPAREN RPAREN block_statement'
+
+def p_statement_list(p):
+    '''statement_list : statement
+                     | statement statement_list'''
+#region statements
 def p_statement_expression_statement(p):
     'statement : expression_statement'
 
@@ -16,6 +22,12 @@ def p_statement_var_declaration(p):
 
 def p_statement_var_assignment(p):
     'statement : var_assignment'
+
+def p_if_statement(p):
+    'statement : IF LPAREN condition RPAREN LBRACE expression RBRACE'
+
+def p_if_else_statement(p):
+    'statement : IF LPAREN condition RPAREN LBRACE expression RBRACE ELSE LBRACE expression RBRACE'
 
 def p_statement_while_statement(p):
     'statement : while_statement'
@@ -28,17 +40,47 @@ def p_statement_for_statement(p):
 
 def p_statement_block_statement(p):
     'statement : block_statement'
+#endregion
 
-def p_expression_statement_semi(p):
+#Region Operadores Relacionais
+def p_condition_notequal(p):
+    'condition : expression NOTEQUAL expression'
+
+def p_condition_greaterequal(p):
+    'condition : expression GREATEREQUAL expression'
+
+def p_condition_lessequal(p):
+    'condition : expression LESSEQUAL expression'
+#-END
+
+#Region Operadores Logicos
+
+def p_condition_and(p):
+    'condition : condition AND condition'
+
+def p_condition_or(p):
+    'condition : condition OR condition'
+
+def p_condition_not(p):
+    'condition : NOT condition'
+
+def p_condition_expr(p):
+    'condition : expression'
+
+def p_condition_paren(p):
+    'condition : LPAREN condition RPAREN'
+#endregion
+
+def p_expression_statement(p):
     'expression_statement : expression SEMICOLON'
 
-def p_var_declaration_semi(p):
+def p_var_declaration(p):
     'var_declaration : LET MUT ID ASSIGN expression SEMICOLON'
 
-def p_var_declaration2_semi(p):
+def p_var_declaration2(p):
     'var_declaration : LET ID ASSIGN expression SEMICOLON'
 
-def p_var_assignment_semi(p):
+def p_var_assignment(p):
     'var_assignment : ID ASSIGN expression SEMICOLON'
 
 def p_while_statement(p):
@@ -47,11 +89,20 @@ def p_while_statement(p):
 def p_for_statement(p):
     'for_statement : FOR ID IN expression block_statement'
 
-def p_return_statement_semi(p):
+def p_return_statement(p):
     'return_statement : RETURN expression SEMICOLON'
 
 def p_block_statement_braces(p):
-    'block_statement : LBRACE program RBRACE'
+    'block_statement : LBRACE statement_list RBRACE'
+
+def p_condition_greater(p):
+    'condition : expression GREATER expression'
+
+def p_condition_less(p):
+    'condition : expression LESS expression'
+
+def p_condition_equals(p):
+    'condition : expression EQUALS expression'
 
 def p_expression_plus(p):
     'expression : expression PLUS term'
@@ -60,6 +111,9 @@ def p_expression_plus(p):
 def p_expression_minus(p):
     'expression : expression MINUS term'
     # p[0] = p[1] - p[3]
+
+def p_term_modulo(p):
+    'term : term MODULO factor'
 
 def p_term_times(p):
     'term : term TIMES factor'
@@ -88,6 +142,10 @@ def p_factor_paren(p):
     'factor : LPAREN expression RPAREN'
     # p[0] = p[2]
 
+def p_factor_boolean(p):
+    '''factor : TRUE
+              | FALSE'''
+
 def p_factor_id(p):
     'factor : ID'
 
@@ -98,20 +156,22 @@ def p_error(p):
 parser = yacc.yacc()
 
 # Teste do parser
-resultado = parser.parse('''
-    let mut x = 5;
-    x + 1;
-    let y = 4;
-    1 + 1;
-    3 - 2;
-    x = y + 4;
-    while (1) {
-      1 + 1;
-      2 / 2;
-    }
-    for x in 1..10 / 5 {
-      0 + 1;
-    }
-    return x;
+resultado = parser.parse('''                 
+    fn main(){
+        let mut x = 5;
+        x + 1;
+        let y = 4;
+        1 + 1;
+        3 - 2;
+        x = y + 4;
+        while (1) {
+        1 + 1;
+        2 / 2;
+        }
+        for x in 1..10 / 5 {
+        0 + 1;
+        }
+        return x;                 
+    }     
 ''')
 print(resultado)
