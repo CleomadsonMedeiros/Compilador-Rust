1ee73da2ad689e27e17253b64643e460754d9056
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2025-03-14 14:37:41 -0300

    Revert "refactor: colocar return nas funções de accept"
    This reverts commit 2f717e3e6c93ad55f816ed3a7847484c0ef242f7.

--- Rust Compiler WIP/AbstractSyntax.py
+++ Rust Compiler WIP/AbstractSyntax.py
@@ -5,26 +5,26 @@ class Program():
   def __init__(self, main):
     self.main = main
   def accept(self, visitor):
-    return visitor.visitProgram(self)
+    visitor.visitProgram(self)
 
 class MainF():
   def __init__(self, blockStatement):
     self.blockStatement = blockStatement
   def accept(self, visitor):
-    return visitor.visitMainFunction(self)
+    visitor.visitMainFunction(self)
 
 class StatementListStatement():
   def __init__(self, statement):
     self.statement = statement
   def accept(self, visitor):
-    return visitor.visitStatementListStatement(self)
+    visitor.visitStatementListStatement(self)
 
 class StatementList():
   def __init__(self, statement, statementList):
     self.statement = statement
     self.statementList = statementList
   def accept(self, visitor):
-    return visitor.visitStatementList(self)
+    visitor.visitStatementList(self)
 
 class DefFunction():
   def __init__(self, id, paramList, returnType, blockStatement):
@@ -33,40 +33,40 @@ class DefFunction():
     self.returnType = returnType
     self.blockStatement = blockStatement
   def accept(self, visitor):
-    return visitor.visitDefFunction(self)
+    visitor.visitDefFunction(self)
 
 class DefFunctionUnit():
   def __init__(self, id, blockStatement):
     self.id = id
     self.blockStatement = blockStatement
   def accept(self, visitor):
-    return visitor.visitDefFunctionUnit(self)
+    visitor.visitDefFunctionUnit(self)
 
 class FunctionCall():
   def __init__(self, id):
     self.id = id
   def accept(self, visitor):
-    return visitor.visitFunctionCall(self)
+    visitor.visitFunctionCall(self)
 
 class FunctionCallIdList():
   def __init__(self, id, idList):
     self.id = id
     self.idList = idList
   def accept(self, visitor):
-    return visitor.visitFunctionCallIdList(self)
+    visitor.visitFunctionCallIdList(self)
 
 class IdListIdNumIdList():
   def __init__(self, idNum, idList):
     self.idNum = idNum
     self.idList = idList
   def accept(self, visitor):
-    return visitor.visitIdListIdNumIdList()
+    visitor.visitIdListIdNumIdList()
 
 class IdListIdNumFunctionCall():
   def __init__(self, idNumFunctionCall):
     self.idNumFunctionCall = idNumFunctionCall
   def accept(self, visitor):
-    return visitor.visitIdListIdNumFunctionCall(self)
+    visitor.visitIdListIdNumFunctionCall(self)
 
 class Expression(ABC):
   @abstractmethod
@@ -98,119 +98,119 @@ class ExpressionAnd(Expression):
     self.expression = expression
     self.condition = condition
   def accept(self, visitor):
-    return visitor.visitExprAnd(self)
+    visitor.visitExprAnd(self)
     
 class ExpressionOr(Expression):
   def __init__(self, expression, condition):
     self.expression = expression
     self.condition = condition
   def accept(self, visitor):
-    return visitor.visitExprOr(self)
+    visitor.visitExprOr(self)
     
 class ExpressionNot(Expression):
   def __init__(self, condition):
     self.condition = condition
   def accept(self, visitor):
-    return visitor.visitExprNot(self)
+    visitor.visitExprNot(self)
 
 class ConditionTerm(Condition):
   def __init__(self, term):
     self.term = term
   def accept(self, visitor):
-    return visitor.visitConditionTerm(self)
+    visitor.visitConditionTerm(self)
     
 class ExpressionStatement(Expression):
   def __init__(self, expression):
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitExpressionStatement(self)
+    visitor.visitExpressionStatement(self)
     
 class VarDeclarationMutId(VarDeclaration):
   def __init__(self, id, expression):
     self.id = id
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitVarDeclarationMutId(self)
+    visitor.visitVarDeclarationMutId(self)
 
 class VarDeclarationMutParam(VarDeclaration):
   def __init__(self, param, expression):
     self.param = param
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitVarDeclarationMutParam(self)
+      visitor.visitVarDeclarationMutParam(self)
 
 class VarDeclarationId(VarDeclaration):
   def __init__(self, id, expression):
     self.id = id
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitVarDeclarationId(self)
+    visitor.visitVarDeclarationId(self)
 
 class VarDeclarationParam(VarDeclaration):
   def __init__(self, param, expression):
     self.param = param
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitVarDeclarationParam(self)
+    visitor.visitVarDeclarationParam(self)
 
 class VarAssignment(VarAssignment):
   def __init__(self, id, expression):
     self.id = id
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitVarAssignment(self)
+    visitor.visitVarAssignment(self)
     
 class WhileStatement(Statement):
   def __init__(self, expression):
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitWhileStatement(self)
+    visitor.visitWhileStatement(self)
 
 class ForStatement(Statement):
   def __init__(self, id, expression):
     self.id = id
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitForStatement(self)
+    visitor.visitForStatement(self)
 
 class ReturnStatement(Statement):
   def __init__(self, expression):
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitReturnStatement(self)
+    visitor.visitReturnStatement(self)
     
 class StatementBlockStatement(Statement):
   def __init__(self, statementList):
     self.statements = statementList
   def accept(self, visitor):
-    return visitor.visitStatementBlockStatement(self)
+    visitor.visitStatementBlockStatement(self)
     
 class ExpressionPlus(Expression):
   def __init__(self, expression, term):
     self.expression = expression
     self.term = term
   def accept(self, visitor):
-    return visitor.visitExpressionPlus(self)
+    visitor.visitExpressionPlus(self)
     
 class ExpressionMinus(Expression):
   def __init__(self, expression, term):
     self.expression = expression
     self.term = term
   def accept(self, visitor):
-    return visitor.visitExpressionMinus(self)
+    visitor.visitExpressionMinus(self)
   
 class ExpressionTerm(Expression):
   def __init__(self, term):
     self.term = term
   def accept(self, visitor):
-    return visitor.visitExpressionTerm(self)
+    visitor.visitExpressionTerm(self)
 
 class ExpressionRange(Expression):
   def __init__(self, expression, term):
     self.expression = expression
     self.term = term
   def accept(self, visitor):
-    return visitor.visitExpressionRange(self)
+    visitor.visitExpressionRange(self)
     
 class Term(ABC):
   @abstractmethod
@@ -222,27 +222,27 @@ class TermModulo(Term):
     self.term = term
     self.factor = factor
   def accept(self, visitor):
-    return visitor.visitTermModulo(self)
+    visitor.visitTermModulo(self)
 
 class TermTimes(Term):
   def __init__(self, term, factor):
     self.term = term
     self.factor = factor
   def accept(self, visitor):
-    return visitor.visitTermTimes(self)
+    visitor.visitTermTimes(self)
 
 class TermDivide(Term):
   def __init__(self, term, factor):
     self.term = term
     self.factor = factor
   def accept(self, visitor):
-    return visitor.visitTermDivide(self)
+    visitor.visitTermDivide(self)
 
 class TermFactor(Term):
   def __init__(self, factor):
     self.factor = factor
   def accept(self, visitor):
-    return visitor.visitTermFactor(self)
+    visitor.visitTermFactor(self)
 
 class Factor(ABC):
   @abstractmethod
@@ -253,34 +253,34 @@ class FactorNumber(Factor):
   def __init__(self, number):
     self.number = number
   def accept(self, visitor):
-    return visitor.visitFactorNumber(self)
+    visitor.visitFactorNumber(self)
 
 class FactorParen(Factor):
   def __init__(self, expression):
     self.expression = expression
   def accept(self, visitor):
-    return visitor.visitFactorParen(self)
+    visitor.visitFactorParen(self)
 
 class FactorBooleanTrue(Factor):
   def __init__(self, true):
     self.true = true
   def accept(self, visitor):
-    return visitor.visitFactorBooleanTrue(self)
+    visitor.visitFactorBooleanTrue(self)
 
 class FactorBooleanFalse(Factor):
   def __init__(self, false):
     self.false = false
   def accept(self, visitor):
-    return visitor.visitFactorBooleanFalse(self)
+    visitor.visitFactorBooleanFalse(self)
 
 class FactorBooleanFalse(Factor):
   def __init__(self, false):
     self.false = false
   def accept(self, visitor):
-    return visitor.visitFactorBooleanFalse(self)
+    visitor.visitFactorBooleanFalse(self)
 
 class FactorID(Factor):
   def __init__(self, id):
     self.id = id
   def accept(self, visitor):
-    return visitor.visitFactorID(self)
+    visitor.visitFactorID(self)
